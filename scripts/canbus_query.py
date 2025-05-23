f6e467b1b344a9f92871a04642e1887b1a482071
AiziChen <1098679804@qq.com>
2025-05-23 22:01:08 +0800

    canbus_query.py: query all of available CAN interfaces with no argument specify
    also added support for quering more than 1 CAN interfaces

--- scripts/canbus_query.py
+++ scripts/canbus_query.py
@@ -4,8 +4,12 @@
 # Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import sys, os, optparse, time
+import sys
+import os
+import optparse
+import time
 import can
+import errno
 
 CANBUS_ID_ADMIN = 0x3f0
 CMD_QUERY_UNASSIGNED = 0x00
@@ -13,12 +17,34 @@ RESP_NEED_NODEID = 0x20
 CMD_SET_KLIPPER_NODEID = 0x01
 CMD_SET_CANBOOT_NODEID = 0x11
 
+
+def get_can_interfaces():
+    try:
+        return [
+            iface
+            for iface in os.listdir("/sys/class/net")
+            if iface.startswith("can")
+        ]
+    except Exception as e:
+        print(
+            f"Error listing `sys/class/net`: {e}, assuming no CAN interface/s."
+        )
+        return []
+
+
 def query_unassigned(canbus_iface):
     # Open CAN socket
     filters = [{"can_id": CANBUS_ID_ADMIN + 1, "can_mask": 0x7ff,
                 "extended": False}]
-    bus = can.interface.Bus(channel=canbus_iface, can_filters=filters,
-                            bustype='socketcan')
+    try:
+        bus = can.interface.Bus(channel=canbus_iface, can_filters=filters,
+                                bustype='socketcan')
+    except OSError as e:
+        if e.errno == errno.ENODEV:
+            sys.stderr.write(f"Interface {canbus_iface} not found!\n")
+            sys.exit(1)
+        raise
+
     # Send query
     msg = can.Message(arbitration_id=CANBUS_ID_ADMIN,
                       data=[CMD_QUERY_UNASSIGNED], is_extended_id=False)
@@ -26,14 +52,14 @@ def query_unassigned(canbus_iface):
     # Read responses
     found_ids = {}
     start_time = curtime = time.time()
-    while 1:
+    while True:
         tdiff = start_time + 2. - curtime
         if tdiff <= 0.:
             break
         msg = bus.recv(tdiff)
         curtime = time.time()
         if (msg is None or msg.arbitration_id != CANBUS_ID_ADMIN + 1
-            or msg.dlc < 7 or msg.data[0] != RESP_NEED_NODEID):
+                or msg.dlc < 7 or msg.data[0] != RESP_NEED_NODEID):
             continue
         uuid = sum([v << ((5-i)*8) for i, v in enumerate(msg.data[1:7])])
         if uuid in found_ids:
@@ -47,18 +73,27 @@ def query_unassigned(canbus_iface):
         if msg.dlc > 7:
             app_id = msg.data[7]
         app_name = AppNames.get(app_id, "Unknown")
-        sys.stdout.write("Found canbus_uuid=%012x, Application: %s\n"
-                         % (uuid, app_name))
+        sys.stdout.write("[%s] Found canbus_uuid=%012x, Application: %s\n"
+                         % (canbus_iface, uuid, app_name))
     sys.stdout.write("Total %d uuids found\n" % (len(found_ids,)))
 
+
 def main():
-    usage = "%prog [options] <can interface>"
+    usage = "%prog [options] [<can interface0> <caninterface1> ...]"
     opts = optparse.OptionParser(usage)
     options, args = opts.parse_args()
-    if len(args) != 1:
-        opts.error("Incorrect number of arguments")
-    canbus_iface = args[0]
-    query_unassigned(canbus_iface)
+
+    if len(args) == 0:
+        query_ifaces = get_can_interfaces()
+        if not query_ifaces:
+            sys.stderr.write("No CAN interfaces found!\n")
+            sys.exit(1)
+    else:
+        query_ifaces = args
+
+    for iface in query_ifaces:
+        query_unassigned(iface)
+
 
 if __name__ == '__main__':
     main()
